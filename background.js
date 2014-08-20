5691fb87020692b461bfa85db8022f90b0e76ab5
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2014-08-20 11:47:27 +0400

    Settings caching

--- background.js
+++ background.js
@@ -6,112 +6,136 @@ var CFFVK;
 CFFVK = CFFVK || (function () {
   "use strict";
 
+  var settings;
+
   // The main function
   function execute(tabId) {
-    chrome.storage.sync.get(null, function (settings) {
-      var cssCode = "";
+    var cssCode = "";
 
-      if (settings.groups === "checked") {
+    if (settings.groups === "checked") {
 
-        if (settings.people === "checked") {
-          cssCode += "div[class^='feed_repost'] {display: none;}";
-        } else {
-          cssCode +=
-            "div[class^='feed_repost'] {display: block;}" +
-            "div[class^='feed_repost-']," +
-            "div[class^='feed_reposts_'] {display: none;}";
-        }
-
-        if (settings.mygroups === "checked") {
-          cssCode += "div[id^=post-].post_copy {display: none;}";
-        } else {
-          cssCode += "div[id^=post-].post_copy {display: block;}";
-        }
-
+      if (settings.people === "checked") {
+        cssCode += "div[class^='feed_repost'] {display: none;}";
       } else {
-        cssCode += "div[class^='feed_repost']," +
-          "div[id^=post-].post_copy {display: block;}";
+        cssCode +=
+          "div[class^='feed_repost'] {display: block;}" +
+          "div[class^='feed_repost-']," +
+          "div[class^='feed_reposts_'] {display: none;}";
       }
 
-      chrome.tabs.insertCSS(tabId, {code: cssCode});
-      chrome.tabs.executeScript(tabId, {code:
-        "CFFVK.clean(" + JSON.stringify(settings) + ");"
-        });
-    });
+      if (settings.mygroups === "checked") {
+        cssCode += "div[id^=post-].post_copy {display: none;}";
+      } else {
+        cssCode += "div[id^=post-].post_copy {display: block;}";
+      }
+
+    } else {
+      cssCode += "div[class^='feed_repost']," +
+        "div[id^=post-].post_copy {display: block;}";
+    }
+
+    chrome.tabs.insertCSS(tabId, {code: cssCode});
+    chrome.tabs.executeScript(tabId, {code:
+      "CFFVK.clean(" + JSON.stringify(settings) + ");"
+      });
   }
 
   // Do things with the second and the third checkboxes:
   function hideOrShowCheckboxes2and3() {
-    chrome.storage.sync.get(null, function (settings) {
-      var form = document.settingsForm,
-        newSettings = {},
-        label,
-        checkbox,
-        i;
+    var form = document.settingsForm,
+      newSettings = {},
+      label,
+      checkbox,
+      i;
 
-      for (i = 2; i < 4; i += 1) {
-        label = form.children[i];
+    for (i = 2; i < 4; i += 1) {
+      label = form.children[i];
 
-        // If the first checkbox ("groups") is unchecked
-        // then uncheck the second and the third, hide them,
-        // and reset their settings in storage:
-        if (settings.groups !== "checked") {
-          label.style.display = "none";
-          checkbox = label.children[0];
-          checkbox.checked = false;
-          newSettings[checkbox.name] = "";
-        } else {
-          label.style.display = "block";
-        }
+      // If the first checkbox ("groups") is unchecked
+      // then uncheck the second and the third, hide them,
+      // and reset their settings in storage:
+      if (settings.groups !== "checked") {
+        label.style.display = "none";
+        checkbox = label.children[0];
+        checkbox.checked = false;
+        settings[checkbox.name] = newSettings[checkbox.name] = "";
+      } else {
+        label.style.display = "block";
       }
+    }
 
-      if (Object.keys(newSettings).length > 0) {
-        chrome.storage.sync.set(newSettings);
-      }
-    });
+    if (Object.keys(newSettings).length > 0) {
+      chrome.storage.sync.set(settings);
+    }
   }
 
   // Catch clicks on checkboxes and remember the values ("checked"),
   // reapply the main function:
   function handleClick(event) {
     var name = event.target.name,
-      value = event.target.value,
-      newSettings = {};
+      value = event.target.value;
 
-    chrome.storage.sync.get(name, function (settings) {
-      newSettings[name] = settings[name] === value ? "" : value;
-      chrome.storage.sync.set(newSettings, function () {
-        hideOrShowCheckboxes2and3();
-        execute();
-      });
+    settings[name] = settings[name] === value ? "" : value;
+    execute();
+    chrome.storage.sync.set(settings, function () {
+      hideOrShowCheckboxes2and3();
     });
   }
 
-  // For older versions: convert localStorage to chrome.storage.sync
+  function setUpTheSettingsPage() {
+    if (document.settingsForm) {
+      var checkboxes = document.settingsForm.getElementsByTagName("input"),
+        checkbox,
+        i;
+
+      hideOrShowCheckboxes2and3();
+
+      for (i = 0; i < checkboxes.length; i += 1) {
+        checkbox = checkboxes[i];
+        checkbox.addEventListener("click", handleClick);
+        if (settings[checkbox.name] === "checked") {
+          checkbox.checked = true;
+        }
+      }
+    }
+  }
+
   if (localStorage.length > 0) {
+
+    // For older versions: convert localStorage to chrome.storage.sync
     (function upgradeStorage() {
-      var newSettings = {},
+      var convertedSettings = {},
         key,
         i;
 
       for (i = 0; i < localStorage.length; i += 1) {
         key = localStorage.key(i);
-        newSettings[key] = localStorage[key];
+        convertedSettings[key] = localStorage[key];
       }
 
-      chrome.storage.sync.set(newSettings, function () {
+      settings = convertedSettings;
+      setUpTheSettingsPage();
+      chrome.storage.sync.set(settings, function () {
         localStorage.clear();
       });
     }());
-  }
+  } else {
 
-  // If there is no saved settings then set them to defaults
-  // (check only the first checkbox):
-  chrome.storage.sync.get(null, function (settings) {
-    if (Object.keys(settings).length === 0) {
-      chrome.storage.sync.set({"groups": "checked"});
-    }
-  });
+    // Load settings. If there are none, set them to defaults
+    // (check only the first checkbox):
+    chrome.storage.sync.get(null, function (loadedSettings) {
+      if (Object.keys(loadedSettings).length === 0) {
+        settings = {
+          groups: "checked"
+        };
+        chrome.storage.sync.set(settings);
+      } else {
+        settings = loadedSettings;
+      }
+
+      setUpTheSettingsPage();
+    });
+  }
 
   return {
 
@@ -149,31 +173,8 @@ CFFVK = CFFVK || (function () {
           }
         }
       }
-    },
-
-    setUpTheSettingsPage: function setUpTheSettingsPage() {
-      var form = document.settingsForm,
-        checkbox,
-        i;
-
-      if (form) {
-        hideOrShowCheckboxes2and3();
-
-        form = form.getElementsByTagName("input");
-        chrome.storage.sync.get(null, function (settings) {
-          for (i = 0; i < form.length; i += 1) {
-            checkbox = form[i];
-            checkbox.addEventListener("click", handleClick);
-            if (settings[checkbox.name] === "checked") {
-              checkbox.checked = true;
-            }
-          }
-        });
-      }
     }
   };
 }());
-
-CFFVK.setUpTheSettingsPage();
 
 chrome.tabs.onUpdated.addListener(CFFVK.checkForValidUrl);
