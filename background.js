8f55608615620f2fe23700ae4b69d730166817ec
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2015-07-26 14:17:21 +0300

    Whitespace

--- background.js
+++ background.js
@@ -1,201 +1,206 @@
-/*jslint browser: true, devel: true, indent: 2 */
+/*jslint browser: true, devel: true */
 
 var chrome;
 
 (function () {
-  "use strict";
+    "use strict";
 
-  var classNames = [
-      "external_links",
-      "links",
-      "apps",
-      "group_share",
-      "mem_link",
-      "event_share",
-      "wall_post_more",
-      "likes",
-      "comments"
-    ],
-    css = {
-      groups: "[id^='feed_repost-'], [id^='feed_reposts_'] { display: none; }",
-      myGroups: "[id^='post-'].post_copy { display: none; }",
-      groupsAndPeople: "[id^='feed_repost'] { display: none; }",
-
-      filters: classNames.map(function (className) {
-        return ".cffvk-" + className;
-      }).join() + "{ display: none; }",
-
-      show: function show(rule) {
-        return rule.replace(/none/g, "block");
-      }
-    },
-    settings = {};
-
-  // The main function
-  function execute(tabId) {
-    var cssCode = "";
-
-    if (settings.groups === "checked") {
-
-      if (settings.people === "checked") {
-        cssCode += css.groupsAndPeople;
-      } else {
-        cssCode += css.show(css.groupsAndPeople) + css.groups;
-      }
-
-      if (settings.mygroups === "checked") {
-        cssCode += css.myGroups;
-      } else {
-        cssCode += css.show(css.myGroups);
-      }
-
-    } else {
-      cssCode += css.show(css.groupsAndPeople + css.myGroups);
-    }
-
-    chrome.tabs.insertCSS(tabId, {code: cssCode});
-    chrome.tabs.executeScript(tabId, {
-      code: "CFFVK.clean(" + JSON.stringify(settings) + ");"
-    });
-  }
-
-  function setUpTheSettingsPage() {
-    var checkboxes;
-
-    function hideOrShowSomeCheckboxes() {
-      var labels2and3 = [
-          document.getElementById("mygroups-label"),
-          document.getElementById("people-label")
+    var classNames = [
+            "external_links",
+            "links",
+            "apps",
+            "group_share",
+            "mem_link",
+            "event_share",
+            "wall_post_more",
+            "likes",
+            "comments"
         ],
-        linksLabel = document.getElementById("links-label"),
-        linksCheckbox = linksLabel.children[0],
-        newSettings = {};
+        css = {
+            groups: "[id^='feed_repost-'], [id^='feed_reposts_'] " +
+                    "{ display: none; }",
+            myGroups: "[id^='post-'].post_copy { display: none; }",
+            groupsAndPeople: "[id^='feed_repost'] { display: none; }",
 
-      // If the first checkbox (`groups`) is unchecked
-      // then uncheck the second and the third, hide them,
-      // and reset their settings in storage:
-      labels2and3.forEach(function (label) {
-        var checkbox = label.children[0];
+            filters: classNames.map(function (className) {
+                return ".cffvk-" + className;
+            }).join() + "{ display: none; }",
 
-        if (settings.groups !== "checked") {
-          label.style.display = "none";
-          checkbox.checked = false;
-          newSettings[checkbox.name] = "";
-          settings[checkbox.name] = "";
+            show: function show(rule) {
+                return rule.replace(/none/g, "block");
+            }
+        },
+        settings = {};
+
+    // The main function
+    function execute(tabId) {
+        var cssCode = "";
+
+        if (settings.groups === "checked") {
+
+            if (settings.people === "checked") {
+                cssCode += css.groupsAndPeople;
+            } else {
+                cssCode += css.show(css.groupsAndPeople) + css.groups;
+            }
+
+            if (settings.mygroups === "checked") {
+                cssCode += css.myGroups;
+            } else {
+                cssCode += css.show(css.myGroups);
+            }
+
         } else {
-          label.style.display = "block";
+            cssCode += css.show(css.groupsAndPeople + css.myGroups);
         }
-      });
 
-      // If the `external_links` checkbox is checked
-      // then uncheck the `links` checkbox, hide it,
-      // and reset its setting in storage:
-      if (settings.external_links === "checked") {
-        linksLabel.style.display = "none";
-        linksCheckbox.checked = false;
-        newSettings[linksCheckbox.name] = "";
-        settings[linksCheckbox.name] = "";
-      } else {
-        linksLabel.style.display = "block";
-      }
-
-      if (Object.keys(newSettings).length > 0) {
-        chrome.storage.sync.set(settings);
-      }
+        chrome.tabs.insertCSS(tabId, {code: cssCode});
+        chrome.tabs.executeScript(tabId, {
+            code: "CFFVK.clean(" + JSON.stringify(settings) + ");"
+        });
     }
 
-    // Catch clicks on checkboxes and remember the values ("checked"),
-    // reapply the main function:
-    function handleClick(event) {
-      var name = event.target.name,
-        value = event.target.value;
+    function setUpTheSettingsPage() {
+        var checkboxes;
 
-      settings[name] = settings[name] === value ? "" : value;
+        function hideOrShowSomeCheckboxes() {
+            var labels2and3 = [
+                    document.getElementById("mygroups-label"),
+                    document.getElementById("people-label")
+                ],
+                linksLabel = document.getElementById("links-label"),
+                linksCheckbox = linksLabel.children[0],
+                newSettings = {};
 
-      chrome.storage.sync.set(settings, function () {
-        hideOrShowSomeCheckboxes();
-        execute();
-      });
-    }
+            // If the first checkbox (`groups`) is unchecked
+            // then uncheck the second and the third, hide them,
+            // and reset their settings in storage:
+            labels2and3.forEach(function (label) {
+                var checkbox = label.children[0];
 
-    if (document.settingsForm) {
-      checkboxes = Array.prototype.slice.call(
-        document.settingsForm.getElementsByTagName("input")
-      );
-
-      hideOrShowSomeCheckboxes();
-
-      checkboxes.forEach(function (checkbox) {
-        checkbox.addEventListener("click", handleClick);
-        if (settings[checkbox.name] === "checked") {
-          checkbox.checked = true;
-        }
-      });
-    }
-  }
-
-  // Launch the main function only on certain pages of VK:
-  function checkForValidUrl(tabId, changeInfo, tab) {
-    var url;
-
-    if (changeInfo.status === "loading") {
-      url = tab.url;
-
-      if (url.indexOf("vk.com/feed") > -1) {
-        if (!(/photos|articles|likes|notifications|comments|updates|replies/)
-            .test(url)) {
-          if (!(/\/feed\?[wz]=/).test(url)) {
-
-            // We have to get the settings on every page load because
-            // `handleClick` works in a different context (popup) and it
-            // doesn't update our `settings` variable
-            chrome.storage.sync.get(function (loadedSettings) {
-              settings = loadedSettings;
-              chrome.tabs.executeScript(
-                tabId,
-                {file: "content-script.js"},
-                function () {
-                  execute(tabId);
+                if (settings.groups !== "checked") {
+                    label.style.display = "none";
+                    checkbox.checked = false;
+                    newSettings[checkbox.name] = "";
+                    settings[checkbox.name] = "";
+                } else {
+                    label.style.display = "block";
                 }
-              );
             });
 
-            chrome.pageAction.show(tabId);
+            // If the `external_links` checkbox is checked
+            // then uncheck the `links` checkbox, hide it,
+            // and reset its setting in storage:
+            if (settings.external_links === "checked") {
+                linksLabel.style.display = "none";
+                linksCheckbox.checked = false;
+                newSettings[linksCheckbox.name] = "";
+                settings[linksCheckbox.name] = "";
+            } else {
+                linksLabel.style.display = "block";
+            }
 
-            chrome.tabs.insertCSS(tabId, {code: css.filters});
-          }
-        } else {
-          chrome.pageAction.hide(tabId);
-
-          // Show all the divs that have been hidden, stop observing:
-          chrome.tabs.insertCSS(tabId, {
-            code: css.show(css.groupsAndPeople + css.myGroups + css.filters)
-          });
-          chrome.tabs.executeScript(tabId, {
-            code:
-              "if (window.CFFVK && CFFVK.observer) {" +
-              "  CFFVK.observer.disconnect();" +
-              "  console.log('CFFVK: cleaning disabled');" +
-              "};"
-          });
+            if (Object.keys(newSettings).length > 0) {
+                chrome.storage.sync.set(settings);
+            }
         }
-      } else {
-        chrome.pageAction.hide(tabId);
-      }
-    }
-  }
 
-  // Load settings. If there are none, set them to defaults
-  // (check only the first checkbox):
-  chrome.storage.sync.get(function (loadedSettings) {
-    if (Object.keys(loadedSettings).length === 0) {
-      settings = {groups: "checked"};
-      chrome.storage.sync.set(settings);
-    } else {
-      settings = loadedSettings;
-    }
-    setUpTheSettingsPage();
-  });
+        // Catch clicks on checkboxes and remember the values ("checked"),
+        // reapply the main function:
+        function handleClick(event) {
+            var name = event.target.name,
+                value = event.target.value;
 
-  chrome.tabs.onUpdated.addListener(checkForValidUrl);
+            settings[name] = settings[name] === value
+                ? ""
+                : value;
+
+            chrome.storage.sync.set(settings, function () {
+                hideOrShowSomeCheckboxes();
+                execute();
+            });
+        }
+
+        if (document.settingsForm) {
+            checkboxes = Array.prototype.slice.call(
+                document.settingsForm.getElementsByTagName("input")
+            );
+
+            hideOrShowSomeCheckboxes();
+
+            checkboxes.forEach(function (checkbox) {
+                checkbox.addEventListener("click", handleClick);
+                if (settings[checkbox.name] === "checked") {
+                    checkbox.checked = true;
+                }
+            });
+        }
+    }
+
+    // Launch the main function only on certain pages of VK:
+    function checkForValidUrl(tabId, changeInfo, tab) {
+        var url;
+
+        if (changeInfo.status === "loading") {
+            url = tab.url;
+
+            if (url.indexOf("vk.com/feed") > -1) {
+                if (!(/photos|articles|likes|notifications|comments|updates|replies/)
+                    .test(url)) {
+                    if (!(/\/feed\?[wz]=/).test(url)) {
+
+                        // We have to get the settings on every page load
+                        // because `handleClick` works in a different context
+                        // (popup) and it doesn't update our `settings` variable
+                        chrome.storage.sync.get(function (loadedSettings) {
+                            settings = loadedSettings;
+                            chrome.tabs.executeScript(
+                                tabId,
+                                {file: "content-script.js"},
+                                function () {
+                                    execute(tabId);
+                                }
+                            );
+                        });
+
+                        chrome.pageAction.show(tabId);
+
+                        chrome.tabs.insertCSS(tabId, {code: css.filters});
+                    }
+                } else {
+                    chrome.pageAction.hide(tabId);
+
+                    // Show all the divs that have been hidden, stop observing:
+                    chrome.tabs.insertCSS(tabId, {
+                        code: css.show(
+                            css.groupsAndPeople + css.myGroups + css.filters
+                        )
+                    });
+                    chrome.tabs.executeScript(tabId, {
+                        code:
+                                "if (window.CFFVK && CFFVK.observer) {" +
+                                "    CFFVK.observer.disconnect();" +
+                                "    console.log('CFFVK: cleaning disabled');" +
+                                "};"
+                    });
+                }
+            } else {
+                chrome.pageAction.hide(tabId);
+            }
+        }
+    }
+
+    // Load settings. If there are none, set them to defaults
+    // (check only the first checkbox):
+    chrome.storage.sync.get(function (loadedSettings) {
+        if (Object.keys(loadedSettings).length === 0) {
+            settings = {groups: "checked"};
+            chrome.storage.sync.set(settings);
+        } else {
+            settings = loadedSettings;
+        }
+        setUpTheSettingsPage();
+    });
+
+    chrome.tabs.onUpdated.addListener(checkForValidUrl);
 }());
--- content-script.js
+++ content-script.js
@@ -1,104 +1,106 @@
-/*jslint browser: true, devel: true, indent: 2 */
+/*global MutationObserver, scroll */
+/*jslint browser: true, devel: true */
 
 var CFFVK;
 
 CFFVK = CFFVK || (function () {
-  "use strict";
+    "use strict";
 
-  var qAndALinks = [
-      "ask.fm",
-      "askbook.me",
-      "askfm.im",
-      "askfm.su",
-      "askmes.ru",
-      "askzone.su",
-      "my-truth.ru",
-      "nekto.me",
-      "otvechayu.ru",
-      "qroom.ru",
-      "sprashivai.by",
-      "sprashivai.ru",
-      "sprashivaii.ru",
-      "sprashivalka.com",
-      "spring.me",
-      "sprosimenya.com",
-      "sprosi.name",
-      "vopros.me",
-      "voprosmne.ru"
-    ],
-    selectorsToFind = {
+    var qAndALinks = [
+            "ask.fm",
+            "askbook.me",
+            "askfm.im",
+            "askfm.su",
+            "askmes.ru",
+            "askzone.su",
+            "my-truth.ru",
+            "nekto.me",
+            "otvechayu.ru",
+            "qroom.ru",
+            "sprashivai.by",
+            "sprashivai.ru",
+            "sprashivaii.ru",
+            "sprashivalka.com",
+            "spring.me",
+            "sprosimenya.com",
+            "sprosi.name",
+            "vopros.me",
+            "voprosmne.ru"
+        ],
+        selectorsToFind = {
 
-      links: qAndALinks.map(function (qAndALink) {
-        return "[href*='" + qAndALink + "']";
-      }).join(","),
+            links: qAndALinks.map(function (qAndALink) {
+                return "[href*='" + qAndALink + "']";
+            }).join(","),
 
-      apps: ".wall_post_source_default",
-      group_share: ".group_share",
-      mem_link: ".mem_link[mention_id^='club']",
-      event_share: ".event_share",
-      external_links: "a[href^='/away.php?to=']:not(.wall_post_source_icon)",
-      wall_post_more: ".wall_post_more",
-      likes: ".post_like_icon.no_likes",
-      comments: ".reply_link"
-    },
-    settings;
+            apps: ".wall_post_source_default",
+            group_share: ".group_share",
+            mem_link: ".mem_link[mention_id^='club']",
+            event_share: ".event_share",
+            external_links:
+                    "a[href^='/away.php?to=']:not(.wall_post_source_icon)",
+            wall_post_more: ".wall_post_more",
+            likes: ".post_like_icon.no_likes",
+            comments: ".reply_link"
+        },
+        settings;
 
-  function processFeedItem(elem, setting, newClassName) {
-    var parent = elem.parentNode;
+    function processFeedItem(elem, setting, newClassName) {
+        var parent = elem.parentNode;
 
-    while (parent !== CFFVK.feed) {
-      if (parent.classList.contains("feed_row")) {
-        if (setting === "checked") {
-          parent.classList.add(newClassName);
-          return;
+        while (parent !== CFFVK.feed) {
+            if (parent.classList.contains("feed_row")) {
+                if (setting === "checked") {
+                    parent.classList.add(newClassName);
+                    return;
+                }
+                parent.classList.remove(newClassName);
+                return;
+            }
+            parent = parent.parentNode;
         }
-        parent.classList.remove(newClassName);
-        return;
-      }
-      parent = parent.parentNode;
     }
-  }
 
-  function find(settingName) {
-    var selector = selectorsToFind[settingName],
-      els = Array.prototype.slice.call(
-        CFFVK.feed.querySelectorAll(selector)
-      ),
-      newClassName = "cffvk-" + settingName;
+    function find(settingName) {
+        var selector = selectorsToFind[settingName],
+            els = Array.prototype.slice.call(
+                CFFVK.feed.querySelectorAll(selector)
+            ),
+            newClassName = "cffvk-" + settingName;
 
-    els.forEach(function (el) {
-      processFeedItem(el, settings[settingName], newClassName);
-    });
-  }
-
-  return {
-    clean: function clean(receivedSettings) {
-      if (receivedSettings) {
-        settings = receivedSettings;
-      }
-      Object.keys(selectorsToFind).forEach(find);
-      console.log("CFFVK: your feed has been cleaned");
-    },
-
-    observer: new window.MutationObserver(function (mutations) {
-      if (mutations[0].addedNodes.length > 0) {
-        CFFVK.clean();
-        console.log("       by the MutationObserver");
-      }
-    }),
-
-    removeInlineStyles: function removeInlineStyles() {
-      var posts = Array.prototype.slice.call(
-        CFFVK.feed.getElementsByClassName("feed_row")
-      );
-
-      posts.forEach(function (post) {
-        post.removeAttribute("style");
-      });
-
-      window.scroll(0, 0);
+        els.forEach(function (el) {
+            processFeedItem(el, settings[settingName], newClassName);
+        });
     }
-  };
+
+    return {
+        clean: function clean(receivedSettings) {
+            if (receivedSettings) {
+                settings = receivedSettings;
+            }
+            Object.keys(selectorsToFind).forEach(find);
+            console.log("CFFVK: your feed has been cleaned");
+        },
+
+        observer: new MutationObserver(function (mutations) {
+            if (mutations[0].addedNodes.length > 0) {
+                CFFVK.clean();
+                console.log("             by the MutationObserver");
+            }
+        }),
+
+        removeInlineStyles: function removeInlineStyles() {
+            var posts = Array.prototype.slice.call(
+                CFFVK.feed.getElementsByClassName("feed_row")
+            );
+
+            posts.forEach(function (post) {
+                post.removeAttribute("style");
+            });
+
+            scroll(0, 0);
+        }
+    };
 }());
 
 CFFVK.feed = document.getElementById("feed_rows");
@@ -106,4 +108,4 @@ CFFVK.feed = document.getElementById("feed_rows");
 CFFVK.observer.observe(CFFVK.feed, {childList: true});
 
 document.getElementById("feed_new_posts").
-  addEventListener("click", CFFVK.removeInlineStyles);
+        addEventListener("click", CFFVK.removeInlineStyles);
