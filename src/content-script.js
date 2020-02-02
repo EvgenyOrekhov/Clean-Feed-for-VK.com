a8c68c24de59d28c524de853b61cf3b599d475ae
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2020-02-02 16:14:47 +0300

    Clean up content-script.js

--- src/content-script.js
+++ src/content-script.js
@@ -1,8 +1,7 @@
-/*global chrome, MutationObserver, scroll, NodeList */
-/*jslint browser, es6, devel, maxlen: 80 */
-/*eslint camelcase: "off", max-statements: "off", no-console: "off" */
-
+// eslint-disable-next-line max-statements
 (function main() {
+  "use strict";
+
   const qAndALinks = [
     "ask.fm",
     "askfm.im",
@@ -14,11 +13,10 @@
     "sprashivalka.com",
     "sprosi.name"
   ];
+
   const selectors = {
     links: qAndALinks
-      .map(function buildSelector(qAndALink) {
-        return `.wall_text [href*="${qAndALink}"]`;
-      })
+      .map(qAndALink => `.wall_text [href*="${qAndALink}"]`)
       .join(),
 
     apps: ".wall_post_source_default",
@@ -28,35 +26,23 @@
     mem_link: ".mem_link[mention_id^='club']",
     event_share: ".event_share",
     external_links:
-      ".wall_text [href^='/away.php?to=']" + ":not(.wall_post_source_icon)",
+      ".wall_text [href^='/away.php?to=']:not(.wall_post_source_icon)",
     wall_post_more: ".wall_post_more,.wall_copy_more",
     likes: ".like.empty",
     comments: ".comment.empty"
   };
 
+  // eslint-disable-next-line fp/no-let
   let feed = document.querySelector("#feed_rows");
+
+  // eslint-disable-next-line fp/no-let, init-declarations
   let settings;
 
-  function find(settingName) {
-    function processFeedRow(feedRow) {
-      const newClassName = `cffvk-${settingName}`;
-
-      if (settings[settingName]) {
-        return feedRow.classList.add(newClassName);
-      }
-
-      feedRow.classList.remove(newClassName);
-    }
-
-    const elements = feed.querySelectorAll(selectors[settingName]);
-
-    elements
-      .map(function getClosestFeedRow(element) {
-        return element.closest(".feed_row");
-      })
-      .filter(function isTruthy(element) {
-        return element;
-      })
+  function toggleCffvkClass([settingName, selector]) {
+    feed
+      .querySelectorAll(selector)
+      .map(element => element.closest(".feed_row"))
+      .filter(Boolean)
       .filter(function isNotAd(element) {
         return (
           element.querySelector(
@@ -64,43 +50,42 @@
           ) === null
         );
       })
-      .forEach(processFeedRow);
+      .forEach(feedRow =>
+        feedRow.classList.toggle(`cffvk-${settingName}`, settings[settingName])
+      );
   }
 
   function clean() {
-    Object.keys(selectors).forEach(find);
+    Object.entries(selectors).forEach(toggleCffvkClass);
     console.log("CFFVK: your feed has been cleaned");
   }
 
   function removeInlineStyles() {
     const posts = feed.querySelectorAll(".feed_row");
 
-    posts.forEach(function removeInlineStyle(post) {
-      post.removeAttribute("style");
-    });
+    posts.forEach(post => post.removeAttribute("style"));
 
     scroll(0, 0);
   }
 
   function startUrlCheck() {
+    // eslint-disable-next-line fp/no-let
     let url = location.href;
 
     const intervalDuration = 100;
 
     function checkUrl() {
       if (url !== location.href) {
+        // eslint-disable-next-line fp/no-mutation
         url = location.href;
-        chrome.runtime.sendMessage({
-          action: "activate"
-        });
+        chrome.runtime.sendMessage({ action: "activate" });
       }
     }
 
     setInterval(checkUrl, intervalDuration);
   }
 
-  NodeList.prototype.forEach =
-    NodeList.prototype.forEach || Array.prototype.forEach;
+  // eslint-disable-next-line fp/no-mutation
   NodeList.prototype.map = NodeList.prototype.map || Array.prototype.map;
 
   const observer = new MutationObserver(function processMutations(mutations) {
@@ -112,15 +97,14 @@
 
   chrome.runtime.onMessage.addListener(function handleMessage(message) {
     if (message.action === "clean") {
+      // eslint-disable-next-line fp/no-mutation
       feed = document.querySelector("#feed_rows");
       observer.disconnect();
-      observer.observe(feed, {
-        childList: true,
-        subtree: true
-      });
+      observer.observe(feed, { childList: true, subtree: true });
       document
         .querySelector("#feed_new_posts")
         .addEventListener("click", removeInlineStyles);
+      // eslint-disable-next-line fp/no-mutation, prefer-destructuring
       settings = message.settings;
 
       return clean();
@@ -132,8 +116,7 @@
     }
   });
 
-  chrome.runtime.sendMessage({
-    action: "activate"
-  });
+  chrome.runtime.sendMessage({ action: "activate" });
+
   startUrlCheck();
 })();
