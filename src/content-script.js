24d6f7e9e13eb632cc9aa883c0196a13039794f3
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2020-03-28 14:29:03 +0300

    Fix hiding posts without likes

--- src/content-script.js
+++ src/content-script.js
@@ -28,7 +28,7 @@
     external_links:
       ".wall_text [href^='/away.php?to=']:not(.wall_post_source_icon)",
     wall_post_more: ".wall_post_more,.wall_copy_more",
-    likes: ".like.empty",
+    likes: ".post_info > .like_wrap .like.empty",
     comments: ".comment.empty",
   };
 
